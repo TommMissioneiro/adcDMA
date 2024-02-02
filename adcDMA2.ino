9bb7580f10c8153feaaea90c3573585d76036345
TommMissioneiro <fabiotomm@gmail.com>
2024-02-02 20:11:17 -0300

    Buffered to 120 Hz -  0 to 3.15V

--- adcDMA2.ino
+++ adcDMA2.ino
@@ -18,7 +18,7 @@ uint32_t min_channel_32, min_channel_33;
 
 static void continuous_adc_init(uint16_t adc1_chan_mask, uint16_t adc2_chan_mask, adc_channel_t *channel, uint8_t channel_num) {
     adc_digi_init_config_t adc_dma_config = {
-        .max_store_buf_size = 200,
+        .max_store_buf_size = 2*TIMES,
         .conv_num_each_intr = TIMES,
         .adc1_chan_mask = adc1_chan_mask,
         .adc2_chan_mask = adc2_chan_mask,
@@ -27,8 +27,8 @@ static void continuous_adc_init(uint16_t adc1_chan_mask, uint16_t adc2_chan_mask
 
     adc_digi_configuration_t dig_cfg = {
         .conv_limit_en = ADC_CONV_LIMIT_EN,
-        .conv_limit_num = 200,
-        .sample_freq_hz = 24 * 1000,
+        .conv_limit_num = TIMES,
+        .sample_freq_hz = 24 * 1000,  //Em quatro canais com buffet de 50 pontos por canal repete a aquisição a cada 120 Hz
         .conv_mode = ADC_CONV_MODE,
         .format = ADC_OUTPUT_TYPE,
     };
@@ -39,7 +39,7 @@ static void continuous_adc_init(uint16_t adc1_chan_mask, uint16_t adc2_chan_mask
    for (int i = 0; i < channel_num; i++) {
         uint8_t unit = GET_UNIT(channel[i]);
         uint8_t ch = channel[i] & 0x7;
-        adc_pattern[i].atten = ADC_ATTEN_DB_0;
+        adc_pattern[i].atten = ADC_ATTEN_DB_11;
         adc_pattern[i].channel = ch;
         adc_pattern[i].unit = unit;
         adc_pattern[i].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
@@ -88,7 +88,4 @@ void loop() {
     read_adc_data();
     printf("Channel 32 - m: %d, Max: %d, Min: %d\n", sum_channel_32>>6, max_channel_32>>6, min_channel_32>>6);
     printf("Channel 33 - m: %d, Max: %d, Min: %d\n", sum_channel_33>>6, max_channel_33>>6, min_channel_33>>6);
-  //  int botao = analogRead(35);
-  //  Serial.print(botao);
-  //  adc_digi_init();
 }
